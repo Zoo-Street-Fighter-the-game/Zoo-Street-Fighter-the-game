03f75c416b4c747a3d45f1b7622c2244e3a0b9b8
dominikkorwek <dominikkorwek@interia.pl>
2023-12-15 16:28:16 +0100

    stworzenie klasy wybieg
    to tylko pokazowe xd

---
+++ game/src/pakiet_klasy_np__wybiegi_pkg/Wybieg.java
@@ -0,0 +1,9 @@
+package pakiet_klasy_np__wybiegi_pkg;
+
+public abstract class Wybieg {
+    /*
+    super by tez bylo, gdyby wszystko bylo opisywane w taki sposob
+    package > klasy zwiazane z package
+    ulatwia to sprawdzanie kodu przez innych
+     */
+}
---
+++ game/src/tests/test1.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class test1 {
+    public static void main(String[] args) {
+         /*
+    zaimplementowana metoda czy cokolwiek
+    powinna zawierac taki test, ktorym pokazane
+    sa wszystkie dodane rzeczy,
+    oraz sprawdzenie ich, czy np nie wywala kodu
+     */
+    }
+}
