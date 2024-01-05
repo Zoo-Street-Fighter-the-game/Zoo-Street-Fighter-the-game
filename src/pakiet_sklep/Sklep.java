8e7843f4e3bf6658060c8d8229b4b06cbd6417e7
dusiaaa <siewieradaria@gmail.com>
2024-01-05 21:33:52 +0100

    Skonczona serializacja

--- src/pakiet_sklep/Sklep.java
+++ src/pakiet_sklep/Sklep.java
@@ -331,10 +331,10 @@ public class Sklep {
             zoo.getZmiennaZasoby().setJedzenie(zoo2.getZmiennaZasoby().getJedzenie());
             zoo.getZmiennaZasoby().setExp(zoo2.getZmiennaZasoby().getExp());
             zoo.setDniCounter(zoo2.getDniCounter());
-            for(int i = 0; i<(zoo2.getListaWybiegow()).size(); i++){
+            for(int i = 0; i<((zoo2.getListaWybiegow()).size()); i++){
                 zoo.dodajWybieg(new Wybieg_podstawowy(zoo2.getListaWybiegow().get(i).getRodzaj_srodowiska(), zoo2.getListaWybiegow().get(i).getWielkosc_wybiegu()));
-                for(int j = 0; j<zoo2.getListaWybiegow().get(i).getLista_zwierzat().size();j++){
-                    zoo.getListaWybiegow().get(i).dodaj_zwierze(new Zwierze(zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(i).getImie(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(i).getNazwa(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(i).getZycie(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(i).getSila(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(i).getSzybkosc(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(i).getSzczescie(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(i).getWielkosc(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(i).getWskaznik_glodu(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(i).getCena(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(i).getRodzaj()));
+                for(int j = 0; j<(zoo2.getListaWybiegow().get(i).getLista_zwierzat().size());j++){
+                    zoo.getListaWybiegow().get(i).dodaj_zwierze(new Zwierze(zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(j).getImie(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(j).getNazwa(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(j).getZycie(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(j).getSila(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(j).getSzybkosc(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(j).getSzczescie(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(j).getWielkosc(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(j).getWskaznik_glodu(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(j).getCena(),zoo2.getListaWybiegow().get(i).getLista_zwierzat().get(j).getRodzaj()));
                 }
 
                 PanelWybieg panelWybieg = new PanelWybieg(zoo, this, zoo.getListaWybiegow().getLast());
