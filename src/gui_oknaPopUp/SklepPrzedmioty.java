14d664d7b759fecb4b68697148fb8f833e22e31f
dusiaaa <siewieradaria@gmail.com>
2024-01-17 09:54:11 +0100

    xd

---
+++ src/gui_oknaPopUp/SklepPrzedmioty.java
@@ -0,0 +1,17 @@
+package gui_oknaPopUp;
+
+import pakiet_sklep.Sklep;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SklepPrzedmioty extends JFrame {
+    private Sklep sklep;
+    public SklepPrzedmioty(Sklep sklep){
+        this.sklep=sklep;
+
+
+    }
+}
+
